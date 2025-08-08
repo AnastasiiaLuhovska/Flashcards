3ee4aac49d263dcede8c3f03982ed5194aa4dd81
Stacy <stacyxberlin@gmail.com>
2025-08-08 11:29:13 +0200

    refactor svg path

--- src/Components/Folder/Folder.tsx
+++ src/Components/Folder/Folder.tsx
@@ -3,7 +3,7 @@ import s from './Folder.module.css'
 const Folder = ({folder}:{folder:FolderType}) => {
     return (
         <li className={s.li}>
-            <svg className={s['icon-wrapper']} width="85" height="60"><use href={'src/assets/symbol-defs.svg#icon-folder'}></use></svg>
+            <svg className={s['icon-wrapper']} width="85" height="60"><use href={'/public/symbol-defs.svg#icon-folder'}></use></svg>
             <p className={s.p}>{folder.name}</p>
         </li>
     );
