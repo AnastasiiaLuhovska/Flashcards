51b2619ac1bfb6b2c3939de8233cf27456e7ae56
Stacy <stacyxberlin@gmail.com>
2025-08-08 11:32:03 +0200

    refactor svg path3

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -3,7 +3,7 @@ import s from './Title.module.css'
 const Title: React.FC<{text:string}> = ({text}) => {
     return (
         <div className={s['title-wrapper']}>
-            <svg width='22' height='22'><use href='src/assets/symbol-defs.svg#icon-star'></use></svg>
+            <svg width='22' height='22'><use href='/symbol-defs.svg#icon-star'></use></svg>
         <p className={s.title}>
             {text}
         </p>
